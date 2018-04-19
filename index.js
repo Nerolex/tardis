16760d120e8f846dbd08687a635d4a585cd8ec69
Michael Glasder <michael.glasder@getit.de>
2018-04-19 18:15:58 +0200

    Changed xml library to synchronous library, refactorings.

--- index.js
+++ index.js
@@ -1,6 +1,24 @@
+//Dependencies
+const { lstatSync, readdirSync } = require('fs');
+const { join } = require('path');
+const { parse } = require('pixl-xml');
+
+const parseXml = file => {
+                            try
+                            {
+                              return (parse(file))
+                            }
+                            catch (error)
+                            {
+                              console.log("Could not parse file " + file + ": " + error);
+                            } 
+                          }
+const isDirectory = source => lstatSync(source).isDirectory();
+const getDirectories = source => readdirSync(source).map(name => join(source, name)).filter(isDirectory);
+
+//Main
 var argv = require('minimist')(process.argv.slice(2));
 console.dir(argv);
-
 /*
 *       PARAMETERS
         -v validate
@@ -19,22 +37,8 @@ if (argv['v'])
     var directory = argv['d'];
     console.log("Starting validation in directory " + directory);
 
-    readXML(directory + '/foo.xml', function(parsedXml) {
-        console.log(parsedXml);
-    });
+    console.dir(getDirectories("."));
+
+    console.log(parseXml(directory + "/foo.xml"));
   }
-}
-
-function readXML(file, callback) {
-  var fileSystem = require('fs');
-  xml2js = require('xml2js');
-
-  console.log("Trying to read file " + file);
-
-  var parser = new xml2js.Parser();
-  fileSystem.readFile(file, function(error, rawXml) {
-    parser.parseString(rawXml, function (error, parsedXml) {
-      callback(parsedXml);
-    });
-  });
 }
