a50f1f15e28f75208b87b64e64b8a85618138c24
Michael Glasder <michael.glasder@getit.de>
2018-04-25 17:34:32 +0200

    Synchronous Functions, Test resources

--- index.js
+++ index.js
@@ -3,18 +3,24 @@ const { lstatSync, readdirSync } = require('fs');
 const { join } = require('path');
 const { parse } = require('pixl-xml');
 
-const parseXml = file => {
+const parseXML = file => {
                             try
                             {
-                              return (parse(file))
+                              return (parse(file, { preserveDocumentNode: true }))
                             }
                             catch (error)
                             {
                               console.log("Could not parse file " + file + ": " + error);
-                            } 
+                            }
                           }
+
+const isSubDirectory = directory => containsXMLFile(directory) && containsChangeLogXML(directory);
+const containsChangeLogXML = directory => getXMLFiles(directory).filter(xmlFile => isChangeLogXML(parseXML(xmlFile))) > 0;
+const isChangeLogXML = xml => xml['databaseChangeLog'] ? true : false;
+const containsXMLFile = path => getXMLFiles(path).length > 0;
 const isDirectory = source => lstatSync(source).isDirectory();
 const getDirectories = source => readdirSync(source).map(name => join(source, name)).filter(isDirectory);
+const getXMLFiles = path => readdirSync(path).filter(file => file.split('.').pop() == 'xml');
 
 //Main
 var argv = require('minimist')(process.argv.slice(2));
@@ -37,8 +43,17 @@ if (argv['v'])
     var directory = argv['d'];
     console.log("Starting validation in directory " + directory);
 
-    console.dir(getDirectories("."));
-
-    console.log(parseXml(directory + "/foo.xml"));
+    if (isSubDirectory(directory))
+    {
+      //Is there an impex for each changelogxml?
+      //Is there a changelogxml for each impex?
+      //are there any left over files?
+      //Are there any two files with the same changelog id?
+    }
+    else
+    {
+      //Should we check for date convention of folder names?
+      //IF we are in the parent directory for the changelogs, go into each changelogfolder and start validation
+    }
   }
 }
